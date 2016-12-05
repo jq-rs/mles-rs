103e6ff021d0f5e2505ad9e165e7c3c011f1f43d
jq-rs <juhamatk@gmail.com>
2016-12-05 22:31:56 +0200

    Unit tests for ChannelDb.

--- mles/src/userchannel.rs
+++ mles/src/userchannel.rs
@@ -10,6 +10,7 @@ use std::collections::HashMap;
 //use tokio_core::net::TcpStream;
 use std::net::TcpStream;
 
+#[derive(Debug)]
 struct ChannelDb<'a> {
     channels: HashMap<String, ChannelDb<'a>>,
     users: HashMap<String, &'a TcpStream>,
@@ -17,7 +18,7 @@ struct ChannelDb<'a> {
 }
 
 impl<'a> ChannelDb<'a> {
-    pub fn join_channel(&'a mut self, socket: &'a TcpStream, userid: &str, channelid: &str) {
+    pub fn join_channel(mut self, socket: &'a TcpStream, userid: &str, channelid: &str) -> ChannelDb<'a> {
         let user_exists = self.check_user_existence(userid);
 
         if !user_exists {
@@ -26,12 +27,13 @@ impl<'a> ChannelDb<'a> {
 
         let channel_exists = self.check_channel_existence(channelid);
         if !channel_exists {
-            let mut channel = ChannelDb{ channels: HashMap::new(), users: HashMap::new(), values: Vec::new() };
+            let channel = ChannelDb{ channels: HashMap::new(), users: HashMap::new(), values: Vec::new() };
             self.channels.insert(channelid.to_string(), channel);
         }
+        self
     }
 
-    pub fn leave_channel(&'a mut self, userid: &str, channelid: &str) {
+    pub fn leave_channel(mut self, userid: &str, channelid: &str) -> ChannelDb<'a> {
         let user_exists = self.check_user_existence(userid);
         if user_exists {
             let channel_exists = self.check_channel_existence(channelid);
@@ -40,21 +42,23 @@ impl<'a> ChannelDb<'a> {
             }
             self.users.remove(userid);
         }
+        self
     }
 
-    fn check_user_existence(&'a self, userid: &str) -> bool {
+    pub fn check_user_existence(&'a self, userid: &str) -> bool {
         let opt = self.users.get(userid);
         match opt {
-            Some(db) => return true,
-                None => {}
+            Some(_) => return true,
+            None => {}
         }
         return false;
     }
-    fn check_channel_existence(&'a self, channelid: &str) -> bool {
+
+    pub fn check_channel_existence(&'a self, channelid: &str) -> bool {
         let opt = self.channels.get(channelid);
         match opt {
-            Some(db) => return true,
-                None => {}
+            Some(_) => return true,
+            None => {}
         }
         return false;
     }
@@ -63,36 +67,72 @@ impl<'a> ChannelDb<'a> {
 #[cfg(test)]
 
 mod tests {
-    use super::*;
+    use std::collections::HashMap;
+    use std::net::TcpStream;
+    use super::ChannelDb;
 
     #[test]
     fn test_join_channel() {
-        let mut user_chan_db = HashMap::new();
-        let channel = "Rust";
-        let user = "Sampo";
-        let mut ret;
+        // google should be up always
+        let socket = TcpStream::connect("209.85.202.94:80").unwrap();
+        {
+            /* Arrange */
+            let mut channel_db = ChannelDb{ channels: HashMap::new(), users: HashMap::new(), values: Vec::new() };
+            let channel = "Rust";
+            let user = "Sampo";
+            let mut ret;
 
-        ret = join_channel(user_chan_db, user, channel);
-        assert_eq!(0, ret);
+            /* Assert */
+            ret = channel_db.check_user_existence(user);
+            assert_eq!(false, ret);
+            ret = channel_db.check_channel_existence(channel);
+            assert_eq!(false, ret);
 
-        ret = join_channel(user_chan_db, user, channel);
-        assert_eq!(1, ret);
+            /* Act */
+            channel_db = channel_db.join_channel(&socket, user, channel);
+
+            /* Assert */
+            ret = channel_db.check_user_existence(user);
+            assert_eq!(true, ret);
+            ret = channel_db.check_channel_existence(channel);
+            assert_eq!(true, ret);
+
+            /* Negative */
+            channel_db = channel_db.join_channel(&socket, user, channel);
+            ret = channel_db.check_user_existence(user);
+            assert_eq!(true, ret);
+            ret = channel_db.check_channel_existence(channel);
+            assert_eq!(true, ret);
+        }
     }
 
+    #[test]
     fn test_leave_channel() {
-        let mut user_chan_db = HashMap::new();
-        let channel = "Evolve";
-        let user = "Anna";
-        let mut ret;
+        // google should be up always
+        let socket = TcpStream::connect("209.85.202.94:80").unwrap();
+        {
+            let mut channel_db = ChannelDb{ channels: HashMap::new(), users: HashMap::new(), values: Vec::new() };
+            let channel = "Mles";
+            let user = "Anna";
+            let mut ret;
 
-        ret = join_channel(user_chan_db, user, channel);
-        assert_eq!(0, ret);
+            channel_db = channel_db.join_channel(&socket, user, channel);
+            ret = channel_db.check_user_existence(user);
+            assert_eq!(true, ret);
+            ret = channel_db.check_channel_existence(channel);
+            assert_eq!(true, ret);
 
-        ret = leave_channel(user_chan_db, user, channel);
-        assert_eq!(0, ret);
+            channel_db = channel_db.leave_channel(user, channel);
+            ret = channel_db.check_user_existence(user);
+            assert_eq!(false, ret);
+            ret = channel_db.check_channel_existence(channel);
+            assert_eq!(false, ret);
 
-        ret = leave_channel(user_chan_db, user, channel);
-        assert_eq!(1, ret);
-
+            channel_db = channel_db.leave_channel(user, channel);
+            ret = channel_db.check_user_existence(user);
+            assert_eq!(false, ret);
+            ret = channel_db.check_channel_existence(channel);
+            assert_eq!(false, ret);
+        }
     }
 }
