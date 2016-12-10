a2d35590528ec4738707f0b6ef71793901cecc7a
jq-rs <juhamatk@gmail.com>
2016-12-10 19:08:09 +0200

    TCP connectivity with futures.

--- mles/src/main.rs
+++ mles/src/main.rs
@@ -1,10 +1,37 @@
 
+extern crate futures;
+extern crate tokio_core;
+
+use futures::stream::Stream;
+use tokio_core::reactor::Core;
+use tokio_core::net::TcpListener;
+use std::collections::HashMap;
+use futures::Future;
+
 mod userchannel;
 
 fn main() {
-        // The statements here will be executed when the compiled binary is called
+    let mut core = Core::new().unwrap();
+    let address = "127.0.0.1:8080".parse().unwrap();
+    let listener = TcpListener::bind(&address, &core.handle()).unwrap();
 
-        // Print text to the console
-        println!("Hello World!");
+    let addr = listener.local_addr().unwrap();
+    println!("Listening for connections on {}", addr);
+
+    let clients = listener.incoming();
+    let welcomes = clients.map(|(socket, _peer_addr)| {
+        {
+            let mut channel_db = userchannel::ChannelDb{ channelname: "Rust".to_string(), users: HashMap::new(), values: Vec::new() };
+            //channel_db = channel_db.join_channel(&socket, "Sampo");
+            //let socket_db = channel_db.users.get_mut("Sampo").unwrap(); 
+            tokio_core::io::write_all(socket, b"Moikka!\n")
+        }
+    });
+    let handle = core.handle();
+    let server = welcomes.for_each(|future| {
+        handle.spawn(future.then(|_| Ok(())));
+        Ok(())
+    });
+
+    core.run(server).unwrap();
 }
-
