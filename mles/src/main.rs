bf49b0e87e2cdf4f610c0a0e29865f407566301e
jq-rs <juhamatk@gmail.com>
2016-12-15 00:43:39 +0200

    Arrangement to have one thread spawned when needed.

--- mles/src/main.rs
+++ mles/src/main.rs
@@ -5,34 +5,49 @@ extern crate serde_derive;
 extern crate futures;
 extern crate tokio_core;
 
-use futures::stream::Stream;
-use tokio_core::reactor::Core;
-use tokio_core::net::TcpListener;
+use std::thread;
+use std::sync::mpsc::channel;
+//use futures::stream::Stream;
+//use tokio_core::reactor::Core;
+//use tokio_core::net::TcpListener;
+use std::net::TcpListener;
 use std::collections::HashMap;
-use futures::Future;
+use std::io::Write;
+//use futures::Future;
 use std::sync::{Arc, Mutex};
 
 mod userchannel;
 mod messaging;
 
 fn main() {
-    let mut core = Core::new().unwrap();
-    let address = "127.0.0.1:8080".parse().unwrap();
-    let listener = TcpListener::bind(&address, &core.handle()).unwrap();
+    let address = "127.0.0.1:8080";
+    let listener = TcpListener::bind(&address).unwrap();
+    let mut cnt = 0;
 
     let addr = listener.local_addr().unwrap();
     println!("Listening for connections on {}", addr);
+    let (tx, rx) = channel();
 
     let mut channel_db = userchannel::ChannelDb{ channelname: "Rust".to_string(), users: HashMap::new(), values: Vec::new() };
 
-    let clients = listener.incoming();
-    let welcomes = clients.and_then(|(socket, _peer_addr)| {
-        //channel_db = channel_db.join_channel(&socket, "Sampo");
-        tokio_core::io::write_all(socket, b"Hello!\n")
-    });
-    let server = welcomes.for_each(|(_socket, _welcome)| {
-        Ok(())
-    });
-
-    core.run(server).unwrap();
+    for socket in listener.incoming() {
+        let mut thr_feed;
+        cnt += 1;
+        if(1 == cnt) {
+            let tx = tx.clone();
+            thread::spawn(move|| {
+                let (thr_tx, thr_rx) = channel();
+                println!("Spawned: Sending socket");
+                loop {
+                    tx.send(thr_tx.clone()).unwrap();
+                    let mut thr_sock: std::net::TcpStream = thr_rx.recv().unwrap();
+                    println!("Got thr sock");
+                    thr_sock.write(b"Hello World\r\n").unwrap();
+                }
+            });
+        }
+        thr_feed = rx.recv().unwrap();
+        println!("Got thr_feed");
+        thr_feed.send(socket.unwrap()).unwrap();
+    }
 }
