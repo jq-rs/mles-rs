05d1204d6f7ed90e0ff4fb4db6a82f6f1df7991e
jq-rs <juhamatk@gmail.com>
2016-12-15 19:30:46 +0200

    Remove dependency of main and channel-thread channels.

--- mles/src/main.rs
+++ mles/src/main.rs
@@ -22,32 +22,41 @@ mod messaging;
 fn main() {
     let address = "127.0.0.1:8080";
     let listener = TcpListener::bind(&address).unwrap();
+    let mut spawned = HashMap::new();
     let mut cnt = 0;
 
     let addr = listener.local_addr().unwrap();
     println!("Listening for connections on {}", addr);
     let (tx, rx) = channel();
 
-    let mut channel_db = userchannel::ChannelDb{ channelname: "Rust".to_string(), users: HashMap::new(), values: Vec::new() };
-
     for socket in listener.incoming() {
-        let mut thr_feed;
+        /* 1. Read incoming msg channel 
+         * 2. If it does not exist, spawn new thread 
+         * 3. Send socket to thread
+         */
         cnt += 1;
         if(1 == cnt) {
             let tx = tx.clone();
             thread::spawn(move|| {
                 let (thr_tx, thr_rx) = channel();
                 println!("Spawned: Sending socket");
+
+                let mut channel_db = userchannel::ChannelDb{ channelname: "Rust".to_string(), users: HashMap::new(), values: Vec::new() };
+
+                tx.send(thr_tx.clone()).unwrap();
                 loop {
-                    tx.send(thr_tx.clone()).unwrap();
                     let mut thr_sock: std::net::TcpStream = thr_rx.recv().unwrap();
                     println!("Got thr sock");
                     thr_sock.write(b"Hello World\r\n").unwrap();
                 }
             });
+            let thr_feed = rx.recv().unwrap();
+            println!("Got thr_feed");
+            thr_feed.send(socket.unwrap()).unwrap();
+            spawned.insert("Channel", thr_feed);
+            continue;
         }
-        thr_feed = rx.recv().unwrap();
-        println!("Got thr_feed");
-        thr_feed.send(socket.unwrap()).unwrap();
+        let thr_socket = spawned.get_mut("Channel").unwrap();
+        thr_socket.send(socket.unwrap()).unwrap();
     }
 }
