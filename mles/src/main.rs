134a32f18dee34966b8d956061785f9372b45732
jq-rs <juhamatk@gmail.com>
2016-12-17 00:29:30 +0200

    Timeout based chatting over Telnet.

--- mles/src/main.rs
+++ mles/src/main.rs
@@ -10,9 +10,12 @@ use std::sync::mpsc::channel;
 //use futures::stream::Stream;
 //use tokio_core::reactor::Core;
 //use tokio_core::net::TcpListener;
+use std::net::TcpStream;
 use std::net::TcpListener;
 use std::collections::HashMap;
 use std::io::Write;
+use std::time::Duration;
+use std::option::Option;
 //use std::io::Read;
 use std::io;
 use std::io::prelude::*;
@@ -28,6 +31,7 @@ fn main() {
     let listener = TcpListener::bind(&address).unwrap();
     let mut spawned = HashMap::new();
     let mut cnt = 0;
+    let option: Option<Duration> = Some(Duration::from_millis(50));
 
     let addr = listener.local_addr().unwrap();
     println!("Listening for connections on {}", addr);
@@ -41,25 +45,46 @@ fn main() {
         let mut result_str = String::new();
         let mut socket = socket.unwrap();
         let mut stream = BufReader::new(socket.try_clone().unwrap());
-        let len  = stream.read_line(&mut result_str);
+        let len = stream.read_line(&mut result_str);
         println!("Got string {}", result_str);
+        socket.set_read_timeout(option);
         if !spawned.contains_key(&result_str) { 
             let tx = tx.clone();
+            let result_str = result_str.clone();
             thread::spawn(move|| {
                 let (thr_tx, thr_rx) = channel();
                 println!("Spawned: Sending socket");
-
-                let mut channel_db = userchannel::ChannelDb{ channelname: "Rust".to_string(), users: HashMap::new(), values: Vec::new() };
-
+                let mut users = HashMap::new();
                 tx.send(thr_tx.clone()).unwrap();
-                let mut thr_sock: std::net::TcpStream = thr_rx.recv().unwrap();
                 println!("Got thr sock");
                 loop {
+                    match thr_rx.try_recv() {
+                        Ok(val) => { 
+                            let thr: TcpStream = val;
+                            cnt += 1;
+                            users.insert(cnt, thr);
+                        },
+                        Err(_) => {}
+                    }
                     let mut bufstr = String::new();
-                    let mut stream = BufReader::new(thr_sock.try_clone().unwrap());
-                    let len = stream.read_line(&mut bufstr);
-
-                    thr_sock.write(&bufstr.into_bytes()).unwrap();
+                    for (user, thr_socket) in &users {
+                        let mut stream = BufReader::new(thr_socket.try_clone().unwrap());
+                        match stream.read_line(&mut bufstr) {
+                            Ok(len) => {
+                                if(len > 0) {
+                                    for (another_user, mut thr_sock) in &users {
+                                        if user != another_user {
+                                            let mut msg = another_user.to_string();
+                                            msg.push_str(":");
+                                            msg.push_str(&bufstr);
+                                            thr_sock.write(&msg.into_bytes()).unwrap();
+                                        }
+                                    }
+                                }
+                            },
+                            Err(_) => {}
+                        }
+                    }
                 }
             });
             let thr_feed = rx.recv().unwrap();
