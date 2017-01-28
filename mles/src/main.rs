05b923470ce2f7a84a584254453b27c27df60ec3
jq-rs <juhamatk@gmail.com>
2017-01-28 14:14:50 +0200

    Tokio support for Mles.

--- mles/src/main.rs
+++ mles/src/main.rs
@@ -1,177 +1,159 @@
+/*
+* Copyright (c) 2016 Alex Crichton
+*
+* Permission is hereby granted, free of charge, to any
+* person obtaining a copy of this software and associated
+* documentation files (the "Software"), to deal in the
+* Software without restriction, including without
+* limitation the rights to use, copy, modify, merge,
+* publish, distribute, sublicense, and/or sell copies of
+* the Software, and to permit persons to whom the Software
+* is furnished to do so, subject to the following
+* conditions:
+*
+* The above copyright notice and this permission notice
+* shall be included in all copies or substantial portions
+* of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
+* ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+* TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+* PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+* SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+* OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+* IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+* DEALINGS IN THE SOFTWARE.
+*/
+
+/* 
+ * Mles asynchnoroues server based on Tokio core example.
+ */
+
+extern crate tokio_core;
+extern crate futures;
 extern crate mles_utils;
 
-use std::thread;
-use std::sync::mpsc::{Sender, Receiver};
-use std::sync::mpsc::channel;
-use std::net::TcpStream;
-use std::net::TcpListener;
 use std::collections::HashMap;
-use std::io::Write;
-use std::time::Duration;
-use std::option::Option;
-use std::str;
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::iter;
+use std::env;
+use std::io::{Error, ErrorKind, BufReader};
+
+use tokio_core::net::TcpListener;
+use tokio_core::reactor::Core;
+use tokio_core::io::{self, Io, Read};
+
+use futures::stream::{self, Stream};
+use futures::Future;
 use mles_utils::*;
 
-
-fn process_channel(tx: Sender<Sender<TcpStream>>, removedtx: Sender<String>, this_channel: String ) {
-    let mut cnt = 0;
-    let (thr_tx, thr_rx): (Sender<TcpStream>, Receiver<TcpStream>) = channel();
-    println!("Spawned: New channel created!");
-    let mut users = HashMap::new();
-    let mut messages: Vec<Vec<u8>> = Vec::new();
-    tx.send(thr_tx.clone()).unwrap();
-    loop {
-        let mut removals = Vec::new();
-        let mut newuser = true;
-        while newuser {
-            match thr_rx.try_recv() {
-                Ok(val) => { 
-                    let mut thr: TcpStream = val;
-                    cnt += 1;
-                    println!("Adding user {}", cnt);
-                    users.insert(cnt, thr.try_clone().unwrap());
-
-                    /* If a new user, all push messages to her */
-                    for buf in &messages {
-                        thr.write(buf.as_slice()).unwrap();
-                    }
-                    newuser = true;
-                },
-                    Err(_) => { newuser = false; }
-            }
-        }
-        for (user, thr_socket) in &users {
-            let stream = thr_socket.try_clone().unwrap();
-            let tuple = read_n(&stream, 4);
-            let status = tuple.0;
-            match status {
-                Ok(0) => {
-                    removals.push(user.clone());
-                },
-                    _ => {}
-            }
-            let mut buf = tuple.1;
-            if 0 == buf.len() {
-                continue;
-            }
-            if read_hdr_type(buf.as_slice()) != 'M' as u32 {
-                continue;
-            }
-            let hdr_len = read_hdr_len(buf.as_slice()) as u64;
-            if 0 == hdr_len {
-                continue;
-            }
-            let tuple = read_n(&stream, hdr_len);
-            let status = tuple.0;
-            match status {
-                Ok(0) => {
-                    removals.push(user.clone());
-                },
-                    _ => {}
-            }
-            let payload = tuple.1;
-            if payload.len() != (hdr_len as usize) {
-                continue;
-            }
-            buf.extend(payload);
-            for (another_user, mut thr_sock) in &users {
-                if user != another_user {
-                    thr_sock.write(buf.as_slice()).unwrap();
-                }
-            }
-            /* Add to local db */
-            messages.push(buf);
-        }
-        for removal in &removals {
-            println!("Removing user {}", removal);
-            users.remove(removal);
-        }
-        if cnt > 0 && users.is_empty() {
-            removedtx.send(this_channel).unwrap();
-            break;
-        }
-    }
-}
+//static mut BUF: Vec<u8> = vec![0,4];
 
 fn main() {
-    let address = "0.0.0.0:8081";
-    let listener = TcpListener::bind(&address).unwrap();
-    let mut spawned = HashMap::new();
-    let option: Option<Duration> = Some(Duration::from_millis(50));
+    let addr = env::args().nth(1).unwrap_or("0.0.0.0:8077".to_string());
+    let addr = addr.parse().unwrap();
+    let mut cnt = 0;
 
-    let addr = listener.local_addr().unwrap();
-    println!("Listening for connections on {}", addr);
-    let (tx, rx) = channel();
-    let (removedtx, removedrx) = channel();
+    // Create the event loop and TCP listener we'll accept connections on.
+    let mut core = Core::new().unwrap();
+    let handle = core.handle();
+    let socket = TcpListener::bind(&addr, &handle).unwrap();
+    println!("Listening on: {}", addr);
 
-    for socket in listener.incoming() {
-        /* Check first has anybody removed channels */
-        let mut remrx = true;
-        while remrx {
-            match removedrx.try_recv() {
-                Ok(val) => { 
-                    let removed_channel: String = val;
-                    println!("Removing unused channel {}", removed_channel.as_str());
-                    spawned.remove(&removed_channel);
-                    remrx = true;
-                },
-                Err(_) => { remrx = false; }
-            }
-        }
-        /* 1. Read incoming msg channel 
-         * 2. If it does not exist, spawn new thread 
-         * 3. Send socket to thread
-         */
-        let socket = socket.unwrap();
-        let stream = socket.try_clone().unwrap();
-        let tuple = read_n(&stream, 4);
-        let status = tuple.0;
-        match status {
-            Ok(0) => {
-               continue;
-            },
-            Ok(_) => {},
-            _ => {
-               continue;
-            },
-        }
-        let buf = tuple.1;
-        if 0 == buf.len() {
-            continue;
-        }
-        if read_hdr_type(buf.as_slice()) != 'M' as u32 {
-            println!("Incorrect payload type");
-            continue;
-        }
-        let tuple = read_n(&stream, read_hdr_len(buf.as_slice()) as u64);
-        let status = tuple.0;
-        match status {
-            Ok(0) => {
-               continue;
-            },
-            Ok(_) => {},
-            _ => {
-               continue;
-            },
-        }
-        let buf = tuple.1;
-        let decoded_msg = message_decode(buf.as_slice());
-        if 0 == decoded_msg.channel.len() {
-            continue;
-        }
-        socket.set_nodelay(true);
-        socket.set_read_timeout(option);
-        if !spawned.contains_key(decoded_msg.channel.as_str()) { 
-            let tx = tx.clone();
-            let removedtx = removedtx.clone();
-            let this_channel = decoded_msg.channel.clone();
-            thread::spawn(move|| process_channel(tx, removedtx, this_channel));
+    // This is a single-threaded server, so we can just use Rc and RefCell to
+    // store the map of all connections we know about.
+    let connections = Rc::new(RefCell::new(HashMap::new()));
 
-            let thr_feed = rx.recv().unwrap();
-            println!("Channel {}", decoded_msg.channel.as_str());
-            spawned.insert(decoded_msg.channel.clone(), thr_feed);
-        }
-        let thr_socket = spawned.get_mut(&decoded_msg.channel).unwrap();
-        thr_socket.send(socket).unwrap();
-    }
+    let srv = socket.incoming().for_each(move |(stream, addr)| {
+        println!("New Connection: {}", addr);
+        let (reader, writer) = stream.split();
+
+        // Create a channel for our stream, which other sockets will use to
+        // send us messages. Then register our address with the stream to send
+        // data to us.
+        let (tx, rx) = futures::sync::mpsc::unbounded();
+        cnt += 1;
+        connections.borrow_mut().insert(cnt, tx);
+
+        // Define here what we do for the actual I/O. That is, read a bunch of
+        // frames from the socket and dispatch them while we also write any frames
+        // from other sockets.
+        let connections_inner = connections.clone();
+        //let reader = BufReader::new(reader);
+
+        // Model the read portion of this socket by mapping an infinite
+        // iterator to each frame off the socket. This "loop" is then
+        // terminated with an error once we hit EOF on the socket.
+        let iter = stream::iter(iter::repeat(()).map(Ok::<(), Error>));
+        let socket_reader = iter.fold(reader, move |reader, _| {
+            // Read a off header the socket, failing if we're at EOF
+            //let frame = io::read_until(reader, b'\n', Vec::new());
+            let mut buf = vec![0,4];
+            let frame = io::read_exact(reader, &mut buf);
+            let frame = frame.and_then(|(reader, payload)| {
+                if payload.len() == 0 {
+                    Err(Error::new(ErrorKind::BrokenPipe, "broken pipe"))
+                } else {
+                    if read_hdr_type(payload.as_slice()) != 'M' as u32 {
+                        return Err(Error::new(ErrorKind::BrokenPipe, "incorrect header"));
+                    }
+                    let hdr_len = read_hdr_len(payload.as_slice());
+                    if 0 == hdr_len {
+                        return Err(Error::new(ErrorKind::BrokenPipe, "incorrect header len"));
+                    }
+                    /* XXX Next read the payload of the frame */
+                    /*let mut payload = vec![0, 100]; //XXX just that it compiles
+                      let buf = io::read_exact(reader, &mut payload);
+                      let buf = buf.and_then(|(reader, buf)| {
+                      if buf.len() == 0 {
+                      return Err(Error::new(ErrorKind::BrokenPipe, "broken pipe"))
+                      } 
+                      Ok((reader, buf))
+                      }); */
+                    Ok((reader, payload))
+                }
+            });
+
+            // Send frame to all other connected clients
+            let connections = connections_inner.clone();
+            frame.map(move |(reader, message)| {
+                println!("{}: {:?}", cnt, message);
+                let mut conns = connections.borrow_mut();
+                let iter = conns.iter_mut()
+                .filter(|&(&k, _)| k != cnt)
+                .map(|(_, v)| v);
+                for tx in iter {
+                    tx.send(message.clone()).unwrap();
+                }
+            reader
+            })
+        });
+
+        // Whenever we receive a string on the Receiver, we write it to
+        // `WriteHalf<TcpStream>`.
+        let socket_writer = rx.fold(writer, |writer, msg| {
+            let amt = io::write_all(writer, msg);
+            let amt = amt.map(|(writer, _)| writer);
+            amt.map_err(|_| ())
+        });
+
+        // Now that we've got futures representing each half of the socket, we
+        // use the `select` combinator to wait for either half to be done to
+        // tear down the other. Then we spawn off the result.
+        let connections = connections.clone();
+        let socket_reader = socket_reader.map_err(|_| ());
+        let connection = socket_reader.map(|_| ()).select(socket_writer.map(|_| ()));
+        handle.spawn(connection.then(move |_| {
+            connections.borrow_mut().remove(&cnt);
+            println!("Connection {} closed.", addr);
+            Ok(())
+        }));
+
+        Ok(())
+    });
+
+    // execute server
+    core.run(srv).unwrap();
 }
