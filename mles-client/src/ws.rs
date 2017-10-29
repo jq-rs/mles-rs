604a44340a2f479e4c84bc0f51fb220ccbc0d254
Juhamatti Kuusisaari <jq-rs@mles.io>
2017-10-29 06:47:32 +0000

    Update CHANGELOG. Bump up version to 1.0.2.

--- mles-client/src/ws.rs
+++ mles-client/src/ws.rs
@@ -60,34 +60,35 @@ pub fn process_ws_proxy(raddr: SocketAddr, keyval: String, keyaddr: String) {
         let keyval_inner = keyval.clone();
         let keyaddr_inner = keyaddr.clone();
         let ws_tx_inner = ws_tx.clone();
-	let callback = |req: &Request| {
-		for &(ref header, ref value) in req.headers.iter() {
-			if header == SECPROT {
-                                let mut secstr = String::new();
-				for c in value.iter() {
-					let c = *c as char;
-                                        secstr.push(c);
-				}
-				//in case several protocol, split to vector of strings
-                                let secvec: Vec<&str> = secstr.split(',').collect();
-                                for secstr in secvec {
-					if secstr == SECVALUE {
-						let extra_headers = vec![
-							(SECPROT.to_string(), SECVALUE.to_string()),
-						];
-						return Ok(Some(extra_headers));
-					}
-				}
-			}
-		}
-                Err(tungstenite::Error::Protocol(Cow::from(SECPROT)))
-	};
+
+        let callback = |req: &Request| {
+            for &(ref header, ref value) in req.headers.iter() {
+                if header == SECPROT {
+                    let mut secstr = String::new();
+                    for c in value.iter() {
+                        let c = *c as char;
+                        secstr.push(c);
+                    }
+                    //in case several protocol, split to vector of strings
+                    let secvec: Vec<&str> = secstr.split(',').collect();
+                    for secstr in secvec {
+                        if secstr == SECVALUE {
+                            let extra_headers = vec![
+                                (SECPROT.to_string(), SECVALUE.to_string()),
+                            ];
+                            return Ok(Some(extra_headers));
+                        }
+                    }
+                }
+            }
+            Err(tungstenite::Error::Protocol(Cow::from(SECPROT)))
+        };
+
         let accept = accept_hdr_async(stream, callback).map_err(|err|{
             println!("Accept error: {}", err);
             Error::new(ErrorKind::Other, err)
         });
         let accept = accept.and_then(move |ws_stream| {
-            let ws_tx_own = ws_tx_inner.clone();
             println!("New WebSocket connection {}: {}", cnt, addr);
 
             thread::spawn(move || ::process_mles_client(raddr, keyval_inner, keyaddr_inner, 
@@ -96,13 +97,9 @@ pub fn process_ws_proxy(raddr: SocketAddr, keyval: String, keyaddr: String) {
             let (sink, stream) = ws_stream.split();
 
             let ws_reader = stream.for_each(move |message: Message| {
-                let ws_tx = ws_tx_own.clone();
                 let mles_tx = mles_tx.clone();
                 let mles_message = message.into_data();
                 let _ = mles_tx.send(mles_message.clone()).wait().map_err(|err| {
-                    Error::new(ErrorKind::Other, err)
-                });
-                let _ = ws_tx.send(mles_message).wait().map_err(|err| {
                     Error::new(ErrorKind::Other, err)
                 });
                 Ok(())
